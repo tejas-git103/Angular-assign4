a7fe1a09ee99c9c4c7036556e07de4f312f01ca0
tejas-git103 <tejasapte47@gmail.com>
2020-06-20 16:41:05 +0530

    basic structure

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+  angular.module('MenuApp')
+  .config(RoutesConfig);
+
+  RoutesConfig.$inject = [ '$stateProvider', '$urlRouterProvider' ];
+  function RoutesConfig($stateProvider, $urlRouterProvider ) {
+    $urlRouterProvider
+    .otherwise('/home');
+    $stateProvider
+    .state('home',{
+      url:'/home',
+      templateUrl:'templates/home.template.html',
+      // template:'<h1>hiiiii</h1>'
+    });
+  }
+}());
